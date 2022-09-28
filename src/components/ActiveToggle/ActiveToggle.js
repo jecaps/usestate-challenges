471bacd3ff995e1a7de5b759a8be2310bfbcfbdd
Jerika Kanz <jerikacapila@gmail.com>
2022-09-28 11:50:12 +0200

    Create function that changes the color of box when active

--- src/components/ActiveToggle/ActiveToggle.js
+++ src/components/ActiveToggle/ActiveToggle.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./ActiveToggle.css";
 
 function ActiveToggle() {
+  const [active, setActive] = useState(false);
+
+  function boxColorHandler() {
+    setActive(!active);
+    const boxEl = document.querySelector(".box");
+    return boxEl.classList.toggle("box--active");
+  }
+
   return (
     <main>
       <div className="box" />
-      <button type="button">Activate</button>
+      <button type="button" onClick={boxColorHandler}>
+        {!active ? "Activate" : "Deactivate"}
+      </button>
     </main>
   );
 }
